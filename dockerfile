e2fe8632993765901ab00fc155af2bd605b039f8
sumit26-lab <sr26rajput@gmail.com>
2025-05-03 22:31:08 +0530

    chnages to port number 80

--- dockerfile
+++ dockerfile
@@ -3,5 +3,5 @@ RUN npm install -g nodemon
 WORKDIR /src
 COPY . .
 RUN npm install
-EXPOSE 3000
+EXPOSE 80
 CMD [ "npm","run" ,"dev"]
--- server.js
+++ server.js
@@ -1,3 +1,3 @@
 const app= require('./app')
 
-app.listen(3000,()=>console.log(`Server runing Port ${3000}`))
+app.listen(80,()=>console.log(`Server runing Port ${80}`))
